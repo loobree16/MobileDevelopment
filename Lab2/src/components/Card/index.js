3ecf35615133aabcaeceb4042bb23ee3d05220b3
Briana Walker <41242641+loobree16@users.noreply.github.com>
2019-09-27 12:43:46 -0400

    Update index.js

--- Lab2/src/components/Card/index.js
+++ Lab2/src/components/Card/index.js
@@ -1,1 +1,11 @@
+import "./index.css";
 
+class Card {
+ constructor(props) {
+   this.props = props;
+ }
+  render() {
+    `<h1 class="title">${this.props.title}</h1>`;
+  }
+}
+export default Card;
