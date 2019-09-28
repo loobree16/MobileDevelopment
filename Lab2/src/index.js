830ce5780a52206975088cac7a99e4492a414e31
Briana Walker <41242641+loobree16@users.noreply.github.com>
2019-09-28 09:25:22 -0400

    Update index.js

--- Lab2/src/index.js
+++ Lab2/src/index.js
@@ -1,5 +1,5 @@
 import Card1 from './src/components/card/index.js';
 
-let card = new Card1({title: "New Title. Much excitement!});
+let card = new Card1({title: "New Title"});
                       
 console.log(card.render() );
