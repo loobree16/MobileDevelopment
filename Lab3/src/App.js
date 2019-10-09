1f4fb02469708924518f5e7eda336f07725d0be7
Briana Walker <41242641+loobree16@users.noreply.github.com>
2019-10-09 13:23:52 -0400

    Create App.js

---
+++ Lab3/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import Card from './components/Card/index.js';
+
+function App() {
+  return (
+    <div className="App">
+        <Card content="This is content" />
+    </div>
+  );
+}
+
+export default App;
